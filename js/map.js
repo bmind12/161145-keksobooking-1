e04038f29e102798b9a42bd1655f3accb676b5fc
Sergey Ryabov <sergey.ryabov@actum.cz>
2018-02-03 14:16:44 +0100

    Add card generation

--- js/map.js
+++ js/map.js
@@ -1,13 +1,30 @@
 'use strict';
 
+// consts
+var APARTMENT_TYPES = {
+  bungalo: 'Бунгало',
+  flat: 'Квартира',
+  house: 'Дом'
+}
+
+// elements
 var map = document.querySelector('.map');
-var ads = generateRandomData(8);
-var fragment = generatePins(ads);
 var mapPins = document.querySelector('.map__pins');
 
-mapPins.appendChild(fragment);
+// data
+var ads = generateRandomData(8);
+
+// fragments
+var pinsFragment = generatePins(ads);
+var cardFragment = generateCard(ads[0]);
+
+// script
+mapPins.appendChild(pinsFragment);
+map.appendChild(cardFragment);
 map.classList.remove('map--faded');
 
+
+// functions
 function generateRandomData(num) {
   var data = [];
   var photoNumbers = [];
@@ -90,6 +107,7 @@ function Ad(
     price: price,
     type: type,
     rooms: rooms,
+    guests: guests,
     checkIn: checkIn,
     checkOut: checkOut,
     features: randomFeatures,
@@ -144,9 +162,10 @@ function generatePins(ads) {
 
   for (var i = 0; i < ads.length; i++) {
     var pin = template.cloneNode(true);
+    var image = pin.querySelector('img');
 
-    pin.style.left = ads[i].location.x + 'px';
-    pin.style.top = ads[i].location.y + 'px';
+    pin.style.left = ads[i].location.x - image.width / 2 + 'px';
+    pin.style.top = ads[i].location.y - image.height + 'px';
     pin.querySelector('img').src = ads[i].author;
 
     fragment.appendChild(pin);
@@ -154,3 +173,64 @@ function generatePins(ads) {
 
   return fragment;
 }
+
+function generateCard(ad) {
+  var template = document.querySelector('template')
+    .content.querySelector('.map__card');
+  var featuresList = template.querySelector('.popup__features');
+  var featuresFragment = generateFeaturesFragment(ad.offer.features);
+  var photosFragment = generatePhotosFragment(ad.offer.photos);
+
+  template.querySelector('h3').innerText = ad.offer.title;
+  template.querySelector('p > small').innerText = ad.offer.address;
+  template.querySelector('.popup__price').innerText =
+    ad.offer.price + '&#x20bd;/ночь';
+  template.querySelector('h4').innerText = APARTMENT_TYPES[ad.offer.type];
+  template.querySelector('h4 ~ p').innerText =
+    ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей ';
+  template.querySelector('h4 ~ p ~ p').innerText =
+  'Заезд после ' + ad.offer.checkIn + ', выезд до ' + ad.offer.checkOut;
+  featuresList.appendChild(featuresFragment);
+  template.querySelector('p:nth-child(5)').innerText = ad.offer.description;
+  template.querySelector('.popup__pictures').appendChild(photosFragment);
+  template.querySelector('.popup__avatar').src = ad.author;
+
+  return template;
+}
+
+function generateFeaturesFragment(features) {
+  var fragment = document.createDocumentFragment();
+  var blockClass = 'feature';
+
+  for (var i = 0; i < features.length; i++) {
+    var specificClass = blockClass + '--' + features[i];
+    var featureItem = document.createElement('li');
+
+    featureItem.classList.add(blockClass, specificClass);
+
+    fragment.appendChild(featureItem);
+  }
+
+  return fragment;
+}
+
+function generatePhotosFragment(photos) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < photos.length; i++) {
+    var photosItem = document.createElement('li');
+    var photosImg = document.createElement('img');
+
+    photosImg.src = photos[i];
+    photosImg.style.width = '50px';
+    photosImg.style.height = '75px';
+    photosImg.style.paddingRight = '10px';
+
+    photosItem.appendChild(photosImg);
+    fragment.appendChild(photosItem);
+  }
+
+  return fragment;
+}
+
+// В список .popup_pictures по аналогии с шаблоном выведите все фотографии из списка фотографий offer.pictures. Каждая из строк массива pictures должна записываться как src соответствующего изображения. Для фотографий нужно явно указать размеры в атрибутах.
